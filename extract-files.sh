3f1bd96b642bfb9c7fa94aeaab14be31dc5e4cdd
Neil <regalstreak@gmail.com>
2014-10-19 19:44:50 +0530

    Added adb pull for /system

--- extract-files.sh
+++ extract-files.sh
@@ -9,6 +9,15 @@ for FILE in `egrep -v '(^#|^$)' proprietary-files.txt`; do
     mkdir -p $BASE/$DIR
   fi
   # My way of pulling blobs without the device
+  cd
+  mkdir android
+  cd android
+  mkdir starpro
+  cd starpro
+  mkdir dump
+  cd dump
+  adb devices
+  adb pull /system
   cp ~/android/starpro/dump/$FILE $BASE/$FILE
 done
 
